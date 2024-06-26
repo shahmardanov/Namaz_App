7c2280fc756a3ef7fde2d5a6797afe1ec8fe870e
shamerdanov <shamil332211@list.ru>
2024-06-26 23:37:16 +0400

    init

--- app/src/main/java/com/example/namaz_app/home/HomeFragment.kt
+++ app/src/main/java/com/example/namaz_app/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.namaz_app.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
---
+++ app/src/main/java/com/example/namaz_app/util/MainApp.kt
@@ -0,0 +1,7 @@
+package com.example.namaz_app.util
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApp : Application()
